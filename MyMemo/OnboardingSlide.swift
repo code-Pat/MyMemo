783b1e263973131d82bebbb46618ede136988077
Donggeun Lee <patlee997@gmail.com>
2021-11-09 22:40:21 +0900

    OnboardingSlide added

---
+++ MyMemo/OnboardingSlide.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingSlide.swift
+//  MyMemo
+//
+//  Created by Donggeun Lee on 2021/11/09.
+//
+
+import UIKit
+
+struct OnboardingSlide {
+    let title: String
+    let description: String
+    let image: UIImage
+    
+}
