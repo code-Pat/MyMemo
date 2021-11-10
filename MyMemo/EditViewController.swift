bc4820296b42ccb8c48e4d53a5347cd9d8459c31
Donggeun Lee <patlee997@gmail.com>
2021-11-10 17:22:09 +0900

    EditViewController updated

--- MyMemo/EditViewController.swift
+++ MyMemo/EditViewController.swift
@@ -6,24 +6,20 @@
 //
 
 import UIKit
+import RealmSwift
 
 class EditViewController: UIViewController {
 
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var textView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
