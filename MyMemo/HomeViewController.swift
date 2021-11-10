7a4d5322b32efb79c4999e068e2059baab522535
Donggeun Lee <patlee997@gmail.com>
2021-11-10 15:38:02 +0900

    homeViewController added

--- MyMemo/HomeViewController.swift
+++ MyMemo/HomeViewController.swift
@@ -9,21 +9,26 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var count2Label: UILabel!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
     }
-    */
-
+    
+    
 }
