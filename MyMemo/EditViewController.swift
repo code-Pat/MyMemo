8c98922e692094bca5dbd7212e5a8227a7cd3b9b
Donggeun Lee <patlee997@gmail.com>
2021-11-10 17:48:50 +0900

    updated

--- MyMemo/EditViewController.swift
+++ MyMemo/EditViewController.swift
@@ -15,10 +15,23 @@ class EditViewController: UIViewController {
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var textView: UITextView!
     
+    var items: Results<MemoData>!
+    
+    let localRealm = try! Realm()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        items = localRealm.objects(MemoData.self)
 
+
+    }
+    @IBAction func saveButtonClicked(_ sender: UIBarButtonItem) {
+        if textView.text.isEmpty {
+            saveButton.isEnabled = false
+        } else {
+            
+        }
     }
     
 
