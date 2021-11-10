1d19f6a4118d82c62a1d72ab83cda73c5a722e64
Donggeun Lee <patlee997@gmail.com>
2021-11-10 16:32:12 +0900

    tableView setup

--- MyMemo/HomeViewController.swift
+++ MyMemo/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 class HomeViewController: UIViewController {
 
@@ -13,21 +14,61 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var countLabel: UILabel!
     @IBOutlet weak var count2Label: UILabel!
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var items: Results<MemoData>!
+    
+    let localRealm = try! Realm()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
+        items = localRealm.objects(MemoData.self)
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
     }
 
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.identifier, for: indexPath) as? HomeTableViewCell
+        else {
+            return UITableViewCell()
+        }
+        
+        let row = items[indexPath.row]
+        
+        cell.contentLabel.text = row.title
+        cell.detailLabel.text = row.content
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        try! localRealm.write {
+            localRealm.delete(items[indexPath.row])
+            tableView.reloadData()
+        }
     }
     
     
